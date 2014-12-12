4cb62918451a69e11d7c7e9d918c0e721c65bc2c
Peide Zhong <pzhong@walmartlabs.com>
2014-12-11 17:45:01 -0800

    first version

---
+++ src/main/java/com/example/lee/MinRotatedSortedArray.java
@@ -0,0 +1,15 @@
+package com.example.lee;
+
+/**
+ * Created by pzhong1 on 12/11/14.
+ * Find Minimum in Rotated Sorted Array
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * Find the minimum element.
+ * You may assume no duplicate exists in the array.
+ */
+public class MinRotatedSortedArray {
+    public int findMin(int[] num) {
+
+    }
+}
