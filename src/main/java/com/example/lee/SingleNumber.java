d8a908d609db90bb54afe4a83f23b1e097066af5
Peide Zhong <pzhong@walmartlabs.com>
2014-12-25 12:44:29 -0800

    add some codes

--- src/main/java/com/example/lee/SingleNumber.java
+++ src/main/java/com/example/lee/SingleNumber.java
@@ -1,5 +1,8 @@
 package com.example.lee;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by benbendaisy on 12/23/14.
  * Given an array of integers, every element appears twice except for one. Find that single one.
@@ -7,7 +10,50 @@ package com.example.lee;
  * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
  */
 public class SingleNumber {
+
+    public int singleNumberI(int[] A) {
+        if(A == null || A.length < 1){
+            return -1;
+        } else if(A.length == 1){
+            return A[0];
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i : A){
+            if(map.containsKey(i)){
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+
+        for(int key : map.keySet()){
+            if(map.get(key) != 2){
+                return key;
+            }
+        }
+
+        return -1;
+    }
+
     public int singleNumber(int[] A) {
-        return 0;
+
+        if(A == null || A.length < 1){
+            return -1;
+        } else if(A.length == 1){
+            return A[0];
+        }
+
+        int temp = 0;
+        for(int i = 0; i < A.length; i++){
+            temp = temp^A[i];
+        }
+
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        SingleNumber singleNumber = new SingleNumber();
+        int[] A = {2, 2, 1};
+        System.out.println(singleNumber.singleNumber(A));
     }
 }
--- src/main/java/com/example/lee/model/RandomListNode.java
+++ src/main/java/com/example/lee/model/RandomListNode.java
@@ -4,7 +4,7 @@ package com.example.lee.model;
  * Created by benbendaisy on 12/23/14.
  */
 public class RandomListNode {
-    int label;
-    RandomListNode next, random;
-    RandomListNode(int x) { this.label = x; }
+    public int label;
+    public RandomListNode next, random;
+    public RandomListNode(int x) { this.label = x; }
 }
