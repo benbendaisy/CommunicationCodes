6852be037f8f5390deaec26d83f6f7e5d6ce475b
Peide Zhong <pzhong@walmartlabs.com>
2015-01-01 11:57:28 -0800

    add 4 codes

---
+++ src/main/java/com/example/lee/model/TreeLinkNode.java
@@ -0,0 +1,10 @@
+package com.example.lee.model;
+
+/**
+ * Created by benbendaisy on 1/1/15.
+ */
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left, right, next;
+    public TreeLinkNode(int x) { val = x; }
+}
